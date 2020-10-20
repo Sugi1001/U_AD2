b72a18f085c3bf06b35bd534088269b0a8339787
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-10-20 15:15:28 +0900

    ステージブロックCppうちました

---
+++ U_AD2/StageBlock.cpp
@@ -0,0 +1,26 @@
+//使用するヘッダーファイル
+#include"GameL/DrawTexture.h"
+#include"GameL/WinInputs.h"
+#include"GameL/SceneManager.h"
+
+#include"GameHead.h"
+#include"StageBlock.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjBlock::Init()
+{
+
+}
+//アクション
+void CObjBlock::Action()
+{
+
+}
+//ドロー
+void CObjBlock::Draw()
+{
+
+}
