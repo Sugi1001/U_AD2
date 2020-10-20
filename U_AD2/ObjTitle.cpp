773257d9792ac7e79019906292bde7b073f4adb6
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-20 16:42:29 +0900

    ObjTitle外側追加

---
+++ U_AD2/ObjTitle.cpp
@@ -0,0 +1,26 @@
+#include"GameL\DrawFont.h"
+#include"GameL\WinInputs.h"
+
+#include"GameHead.h"
+#include"ObjTitle"
+
+//使用するネームペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjTitle::Init()
+{
+
+}
+
+//アクション
+void CObjTitle::Action()
+{
+
+}
+
+//ドロー
+void CObjTitle::Draw()
+{
+
+}
