e3dd3d0a99d8afd31a7b12d5ad0c8cea1543714f
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-20 15:29:30 +0900

    ゲームタイトル一部打ちました（未完成）

---
+++ U_AD2/GameTitle.cpp
@@ -0,0 +1,10 @@
+//ＳＬＴデバック機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GeamLで使用するヘッダー
+#include"GameL\SceneObjManager.h"
+
+//使用するヘッダー
+#include"SceneMain.h"
+#include"GameHead.h"
