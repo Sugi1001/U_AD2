2bfef478c122d6827f341ef15fd1126c93f22092
友一 杉下 <h1202019001@hiratagakuen.onmicrosoft.com>
2020-10-20 15:34:52 +0900

    ObjEnemyBlock(落石)追加します

--- U_AD2/ObjEnemyBlock.h
+++ U_AD2/ObjEnemyBlock.h
@@ -5,4 +5,33 @@
 //使用するネームスペース
 using namespace GameL;
 
-//オブジェクト : プロジェクト
+//オブジェクト : プロジェクト&背景
+class CObjEnemyBlock : public CObj
+{
+public :
+	CObjEnemyBlock() {};
+	~CObjEnemyBlock() {};
+	void Init();  //イニシャライズ
+	void Action(); //アクション
+	void Draw();   //ドロー
+	
+private:
+	float m_px;  //位置
+	float m_py;   //移動ベクトル
+	float m_vx;
+	float m_vy;  //姿勢
+	float m_posture;
+
+	int m_ani_time;  //アニメーションフレーム動作間隔
+	int m_ani_frame; //描画フレーム
+
+	float m_speed_power; //スピードパワー
+	float m_ani_max_time; //アニメーション動作間隔最大値
+
+	//blockとの衝突状態確認用
+	bool m_hit_up;
+	bool m_hit_down;
+	bool m_hit_left;
+	bool m_hit_right;
+
+};
