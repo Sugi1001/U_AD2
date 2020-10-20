f275beed01c81eaebd4aa3afa2241feba64a6c2d
猛 岩永 <h1202015006@hiratagakuen.onmicrosoft.com>
2020-10-20 16:30:26 +0900

    ObjHero.cpp ObjHero.hを変更しました。

--- U_AD2/ObjHero.cpp
+++ U_AD2/ObjHero.cpp
@@ -16,20 +16,49 @@ void CObjHero::Init()
 	m_py = 0.0f;
 	m_vx = 0.0f;//移動ベクトル
 	m_vy = 0.0f;
+	m_posture = 0.0f; //右向き0.0f　左向き1.0f
+
+	m_ani_time = 0;
+	m_ani_frame = 1;//静止フレームを初期にする
+
 }
 
 //アクション
 void CObjHero::Action()
 {
 	//移動ベクトル
-	m_vx = 0.0f;
 	m_vy = 0.0f;
 
 	//キーの入力方向
 	if (Input::GetVKey(VK_RIGHT) == true)
 	{
-		m_vx = -1.0f;
+		m_vx += m_speed_power;
+		m_posture = 1.0f;
+		m_ani_time += 1;
 	}
+    else if (Input::GetVKey(VK_RIGHT) == true)
+	{
+		m_vx -= m_speed_power;
+		m_posture = 0.0f;
+		m_ani_time += 1;
+	}
+	else
+	{
+		m_ani_frame = 1; //キーが入力が無い場合静止フレームにする
+		m_ani_time = 0;
+	}
+	if (m_ani_time > m_ani_max_time)
+	{
+		m_ani_frame += 1;
+		m_ani_time = 0;
+	}
+	if (m_ani_frame == 4)
+	{
+		m_ani_frame = 0;
+	}
+
+	//摩擦
+	m_vx += -(m_vx * 0.098);
 
 	//位置の更新
 	m_px += m_vx;
@@ -40,15 +69,15 @@ void CObjHero::Action()
 void CObjHero::Draw()
 {
 	//描画カラー情報
-	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+	float AniData[4] = { 1,0,2,0};
 
 	RECT_F src; //描画元切り取り位置
 	RECT_F dst; //描画先表示位置
 
 	//切り取り位置の設定
 	src.m_top = 0.0f;
-	src.m_left - 0.0f;
-	src.m_right = 64.0f;
+	src.m_left = 0.0f + AniData[m_ani_frame] * 64;
+	src.m_right = 64.0f + AniData[m_ani_frame] * 64;
 	src.m_bottom = 64.0f;
 
 	//表示位置の設定
@@ -58,5 +87,5 @@ void CObjHero::Draw()
 	dst.m_bottom = 64.0f;
 
 	//描画
-	Draw::Draw(0.&src, &dst, c, 0.0f);
+	Draw::Draw(0,&src, &dst, c, 0.0f);
 }
--- U_AD2/ObjHero.h
+++ U_AD2/ObjHero.h
@@ -1,6 +1,6 @@
 #pragma once
 //使用するヘッダー
-#include "GameL\SceneObjManaer.h" 
+#include "GameL\SceneObjManager.h" 
 //使用するネームスペース
 using namespace GameL;
 
@@ -18,4 +18,11 @@ private:
 	float m_py;
 	float m_vx; //移動ベクトル
 	float m_vy;
+	float m_posture; //姿勢
+
+	int m_ani_time;//アニメーションフレーム動作間隔
+	int m_ani_frame;//描画フレーム
+
+	float m_speed_power = 0.5f;//通常速度
+	float m_ani_max_time;//アニメーション間隔幅
 };
