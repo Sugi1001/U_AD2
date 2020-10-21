1964d3250f94556e76e50070eb5a1b926898dc6d
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-21 10:50:39 +0900

    オブジェヘッダー　記載

--- U_AD2/ObjTitle.h
+++ U_AD2/ObjTitle.h
@@ -1,1 +1,17 @@
 #pragma once
+//使用するヘッダー
+#include"GameL\SceneObjManager.h"
+//使用するネームペース
+using namespace GameL;
+
+//オブジェクト：タイトル
+class CObjTitle :public CObj
+{
+public:
+	CObjTitle() {};
+	~CObjTitle() {};
+	void Init();
+	void Action();
+	void Draw();
+private:
+};
