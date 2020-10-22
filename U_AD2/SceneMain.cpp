5e914aae9146705a74682a2009880823b3e7acf3
友一 杉下 <h1202019001@hiratagakuen.onmicrosoft.com>
2020-10-22 16:32:05 +0900

    エラー解消

--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -27,16 +27,12 @@ CSceneMain::~CSceneMain()
 //初期化メソッド
 void CSceneMain::InitScene()
 {
-	//グラフィック読み込み
-	Draw::LoadImageW(L"背景.png",0,TEX_SIZE_512);
-	//外部データの読み込み(ステージ情報)
-	unique_ptr<wcha_t>_tp; //ステージ情報ポインター
-	int size;   //ステージ情報の大きさ
-	p = save::ExternalDataOpen(L"", &size); //外部データ読み込み
+	
 
 	//blockオブジェクト作成
 	CObjBlock* objb = new CObjBlock();
 	Objs::InsertObj(objb, OBJ_BLOCK, 9);
+
 }
 
 //実行中メソッド
