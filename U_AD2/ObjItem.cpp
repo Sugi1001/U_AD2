af7be8b5b253da2f0e3b7fceb995f8c15d5583ef
隆平 藤田 <h1202015011@hiratagakuen.onmicrosoft.com>
2020-10-22 15:55:12 +0900

    アイテム

--- U_AD2/ObjItem.cpp
+++ U_AD2/ObjItem.cpp
@@ -1,15 +1,32 @@
 //使用するヘッダファイル
+#include"GameL\DrawTexture.h"
+#include"GameL/WinInputs.h"
+#include"GameL/SceneManager.h"
+
 #include"GameHead.h"
 #include"ObjItem.h"
 
-#include"GameL\SceneObjManager.h"
 
 //使用するネームペース
 using namespace GameL;
 
-void Objitem
+//イニシャライズ
+void CObjItem::Init()
+{
 
+}
 
+//アクション
+void CObjItem::Action()
+{
+
+}
+
+//ドロー
+void CObjItem::Draw()
+{
+
+}
 
 
 
