1a2c048ef9d2093ce29fb96993a205bc93c45092
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-10-26 14:45:12 +0900

    外部データの設定をしました

--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -5,6 +5,8 @@
 //GameLで使用するヘッダー
 #include "GameL\SceneObjManager.h"
 #include "GameL\DrawTexture.h"
+#include"GameL/DrawFont.h"
+#include"GameL/UserData.h"
 
 //使用するネームスペース
 using namespace GameL;
@@ -28,6 +30,12 @@ CSceneMain::~CSceneMain()
 //初期化メソッド
 void CSceneMain::InitScene()
 {
+	//外部データの読み込み（ステージ情報）
+	unique_ptr<wchar_t>p;//ステージ情報ポインター
+	int size;//ステージ情報の大きさ
+	p = Save::ExternalDataOpen(L"stage.csv", &size);//外部データ読み込み
+
+
 	//グラフィック読み込み
 	Draw::LoadImageW(L"image.png", 0, TEX_SIZE_512);
 
