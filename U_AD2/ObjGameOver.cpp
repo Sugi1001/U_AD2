3ae1d49cc0f115b3b72bd7e7b37252a7d5925f8b
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-26 14:28:20 +0900

    ObjGameOvercpp中身追加

---
+++ U_AD2/ObjGameOver.cpp
@@ -0,0 +1,25 @@
+#include"GameL\DrawFont.h"
+#include"GameL\WinInputs.h"
+#include"GameL\SceneManager.h"
+
+#include"GameHead.h"
+#include"ObjGameOver.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjGameOver::Init()
+{
+
+}
+
+void CObjGameOver::Action()
+{
+
+}
+
+void CObjGameOver::Draw()
+{
+
+}
