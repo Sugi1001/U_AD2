8c73723590c1d62860788788484c686dc0399a6b
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-26 15:24:50 +0900

    シーンオブジェ変更

--- U_AD2/SceneGameOver.cpp
+++ U_AD2/SceneGameOver.cpp
@@ -1,6 +1,6 @@
 //SLTデバック機能をOFFにする
-#define _SECURE_SCL(0)
-#define _HAS_ITERATOR_DEBUGGING(0)
+#define _SECURE_SCL　(0)
+#define _HAS_ITERATOR_DEBUGGING　(0)
 
 //GameLで使用するヘッダー
 #include "GameL\SceneObjManager.h"
