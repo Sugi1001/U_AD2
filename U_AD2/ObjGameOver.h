320b046d7c4cfede20cff9e13d8085a973fe3beb
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-26 14:19:50 +0900

    ObjGaemOverヘッダー中身追加

--- U_AD2/ObjGameOver.h
+++ U_AD2/ObjGameOver.h
@@ -1,1 +1,17 @@
 #pragma once
+//使用するヘッダー
+#include"GameL\SceneManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：ゲームオーバー
+class CObjGameOver :public CObj
+{
+public:
+	CObjGameOver() {};
+	~CObjGameOver() {};
+	void Init();
+	void Action();
+	void Draw();
+private:
+};
