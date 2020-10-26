1e2d9d58f45a6916593d8307db058e48fb6014bd
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-26 13:37:37 +0900

    ObjTitle 追加

--- U_AD2/ObjTitle.cpp
+++ U_AD2/ObjTitle.cpp
@@ -24,4 +24,6 @@ void CObjTitle::Draw()
 {
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
 	Font::StrDraw(L"アンダーアドベンチャー", 230, 250, 32, c);
+
+	Font::StrDraw(L"GameStart:PushEnterKey", 200, 400, 32, c);
 }
