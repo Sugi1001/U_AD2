c334b9f4a68a2ef1ecffcaad4e4c64d6459dced5
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-26 14:40:22 +0900

    ObjGameOver中身追加

--- U_AD2/ObjGameOver.cpp
+++ U_AD2/ObjGameOver.cpp
@@ -14,9 +14,14 @@ void CObjGameOver::Init()
 
 }
 
+//アクション
 void CObjGameOver::Action()
 {
-
+	//エンターキーを押してシーン：ゲームTitleに移行する
+	if (Input::GetVKey(VK_RETURN) == true)
+	{
+		Scene::SetScene(new CSceneTitle());
+	}
 }
 
 void CObjGameOver::Draw()
