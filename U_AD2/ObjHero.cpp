672259d9cb840b8c33095dfa2e19b6ed3d62b840
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-10-27 15:48:40 +0900

    a

--- U_AD2/ObjHero.cpp
+++ U_AD2/ObjHero.cpp
@@ -90,11 +90,11 @@ void CObjHero::Action()
 
 
 	//ブロックとの当たり判定実行
-	/*CObjBlock* pb = (CObjBlock*)Objs::GetObj(OBJ_BLOCK);
-	pb->BlockHit(&m_px, &m_py, true,
-		&m_hit_up, &m_hit_down, &m_hit_left, &m_hit_right, &m_vx, &m_vy,
-		&m_block_type
-	);*/
+	//CObjBlock* pb = (CObjBlock*)Objs::GetObj(OBJ_BLOCK);
+	//pb->BlockHit(&m_px, &m_py, true,
+		//&m_hit_up, &m_hit_down, &m_hit_left, &m_hit_right, &m_vx, &m_vy,
+		//&m_block_type
+	//);
 
 
 	//位置の更新
