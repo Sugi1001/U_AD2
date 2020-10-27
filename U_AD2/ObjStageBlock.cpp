a78f6573dfa5b8582eb59a2f24bc13087df2b4ff
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-10-27 16:01:31 +0900

    あ

--- U_AD2/ObjStageBlock.cpp
+++ U_AD2/ObjStageBlock.cpp
@@ -2,10 +2,10 @@
 #include"GameL/DrawTexture.h"
 #include"GameL/WinInputs.h"
 #include"GameL/SceneManager.h"
-
+#include"GameL\SceneObjManager.h"
 #include"GameHead.h"
 #include"ObjStageBlock.h"
-#include"GameL\SceneObjManager.h"
+
 
 //使用するネームスペース
 using namespace GameL;
