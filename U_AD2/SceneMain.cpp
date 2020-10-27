79f8423a042c7ca8221ba057b3a602fc14f18ea8
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-10-27 14:21:02 +0900

    背景なおしました

--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -59,7 +59,7 @@ void CSceneMain::InitScene()
 	Draw::LoadImage(L"主人公（移動　逆）.png", 1, TEX_SIZE_512);
 
 	Draw::LoadImage(L"地面.png", 2, TEX_SIZE_512);
-	Draw::LoadImage(L"地面.png", 0, TEX_SIZE_512);
+	//Draw::LoadImage(L"地面.png", 0, TEX_SIZE_512);
 
 
 	
