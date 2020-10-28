4c129a221aaa1cd091f06105db3c08aac4a48ddd
友一 杉下 <h1202019001@hiratagakuen.onmicrosoft.com>
2020-10-28 10:09:23 +0900

    po

--- U_AD2/ObjHero.cpp
+++ U_AD2/ObjHero.cpp
@@ -156,7 +156,7 @@ void CObjHero::Draw()
 	dst.m_bottom = 30.0f + m_py;*/
 
 	////•`‰æ
-	Draw::Draw(1, &src, &dst, c, 1.0f);
+	Draw::Draw(10, &src, &dst, c, 1.0f);
 	//Draw::Draw(2, &src, &dst, c, 0.0f);
 	////Draw::Draw(0, &src, &dst, c, 0.0f);
 	////Draw::Draw(0, &src, &dst, c, 1.0f);
