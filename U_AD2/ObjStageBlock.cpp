fc43ce9df67df37fc90002c12ff44b5f9ed8cffc
猛 岩永 <h1202015006@hiratagakuen.onmicrosoft.com>
2020-10-29 15:11:48 +0900

    マージ解決

--- U_AD2/ObjStageBlock.cpp
+++ U_AD2/ObjStageBlock.cpp
@@ -41,7 +41,7 @@ void CObjStageBlock::Init()
 void CObjStageBlock::Action()
 {
 	//ブロックとの当たり判定実行
-	CObjBlock* pb = (CObjBlock*)Objs::GetObj(OBJ_STAGE_BLOCK);
+	CObjBlock::CObjBlock* pb = (CObjBlock*)Objs::GetObj(OBJ_STAGE_BLOCK);
 
 	//主人公の位置を取得
 	CObjHero* hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
