17dffe57a77f58234d51774f5cd1217781638d54
友一 杉下 <h1202019001@hiratagakuen.onmicrosoft.com>
2020-11-02 16:20:26 +0900

    主人公逆のpngの削除 主人公.pngを採用

--- U_AD2/ObjHero.cpp
+++ U_AD2/ObjHero.cpp
@@ -136,8 +136,8 @@ void CObjHero::Draw()
 
 	////Ø‚èæ‚èˆÊ’u‚Ìİ’è
 
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
+	src.m_top = 64.0f;
+	src.m_left = 64.0f;
 	src.m_right = 64.0f;
 	src.m_bottom = 64.0f;
 
