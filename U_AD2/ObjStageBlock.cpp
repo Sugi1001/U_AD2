332809ec40135b7f089b8658417492041e47a9d4
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-11-02 15:25:49 +0900

    強制スクロールｗ

--- U_AD2/ObjStageBlock.cpp
+++ U_AD2/ObjStageBlock.cpp
@@ -170,7 +170,7 @@ void CObjStageBlock::Draw()
 //src.m_right=src.m_left+64.0f;
 //src.m_bottom=64.0f;
 
-    //m_scroll+=3.0f;//スクロール実験用
+    m_scroll-=1.0f;//スクロール実験用
 
 	for (int i = 0; i < 10; i++)
 	{
