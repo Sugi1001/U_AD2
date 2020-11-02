f0cdcc1d4af045416e185a0eb6090dae3c02ccaa
友一 杉下 <h1202019001@hiratagakuen.onmicrosoft.com>
2020-11-02 14:26:44 +0900

    主人公表示

--- U_AD2/ObjHero.cpp
+++ U_AD2/ObjHero.cpp
@@ -200,7 +200,7 @@ void CObjHero::Draw()
 
 	////•`‰æ
 	//Draw::Draw(1, &src, &dst, c, 0.0f);
-	Draw::Draw(1, &src, &dst, c, 0.0f);
+	Draw::Draw(10, &src, &dst, c, 0.0f);
 	//Draw::Draw(2, &src, &dst, c, 0.0f);
 	////Draw::Draw(0, &src, &dst, c, 0.0f);
 	////Draw::Draw(0, &src, &dst, c, 1.0f);
--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -62,8 +62,8 @@ void CSceneMain::InitScene()
 
 	////Draw::LoadImage(L"主人公.png", 1, TEX_SIZE_512);
 	//Draw::LoadImage(L"主人公（移動　逆）.png", 1, TEX_SIZE_512);
-	Draw::LoadImage(L"主人公（移動　逆）.png", 1, TEX_SIZE_512);
-	//Draw::LoadImage(L"主人公.png", 10, TEX_SIZE_512);
+	//Draw::LoadImage(L"主人公（移動逆.png", 10, TEX_SIZE_512);
+	Draw::LoadImage(L"主人公.png", 1, TEX_SIZE_512);
 
 	//Draw::LoadImage(L"主人公（移動　逆）.png", 1, TEX_SIZE_512);
 	//Draw::LoadImage(L"主人公（移動　逆）.png", 2, TEX_SIZE_512);
