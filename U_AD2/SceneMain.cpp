baaba3261f6ad86d8f2b61cd7bfd6873286fd3c7
猛 岩永 <h1202015006@hiratagakuen.onmicrosoft.com>
2020-11-02 14:27:29 +0900

    コミット

--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -64,10 +64,10 @@ void CSceneMain::InitScene()
 	//Draw::LoadImage(L"主人公（移動　逆）.png", 1, TEX_SIZE_512);
 	Draw::LoadImage(L"主人公（移動　逆）.png", 1, TEX_SIZE_512);
 	//Draw::LoadImage(L"主人公.png", 10, TEX_SIZE_512);
-
+	Draw::LoadImage(L"アンダーアドベンチャー", 10, TEX_SIZE_512);
 	//Draw::LoadImage(L"主人公（移動　逆）.png", 1, TEX_SIZE_512);
 	//Draw::LoadImage(L"主人公（移動　逆）.png", 2, TEX_SIZE_512);
-
+	Draw::LoadImage(L"アンダーアドベンチャー.png", 10, TEX_SIZE_512);
 	//Draw::LoadImage(L"地面.png", 2, TEX_SIZE_512);
 	Draw::LoadImage(L"地面.png", 2, TEX_SIZE_512);
 	//Draw::LoadImage(L"地面.png", 2, TEX_SIZE_512);
@@ -85,6 +85,7 @@ void CSceneMain::InitScene()
 	CObjStageBlock* objb = new CObjStageBlock(map);
 	Objs::InsertObj(objb, OBJ_STAGE_BLOCK, 2);
 
+	
 
    
 
