b1641ddf6b12dd66f5fe74fb71ccf7f091dad5f0
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-11-10 16:12:56 +0900

    画像追加

--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -102,6 +102,7 @@ void CSceneMain::InitScene()
 
 	////グラフィック読み込み
 	Draw::LoadImage(L"背景.png", 0, TEX_SIZE_512);
+	Draw::LoadImage(L"taitle.png", 0, TEX_SIZE_512);
 
 	//Draw::LoadImage(L"主人公.png", 1, TEX_SIZE_512);
 	Draw::LoadImage(L"主人公 (移動 逆) .png", 1, TEX_SIZE_512);
