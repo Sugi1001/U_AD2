3f8e931d40779020511653d7f83c5c43eb98b4ef
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-11-10 13:14:25 +0900

    変更なし

--- U_AD2/ObjStageBlock1.h
+++ U_AD2/ObjStageBlock1.h
@@ -42,8 +42,4 @@ private:
 	//ü‚Æü‚ÆŒğ·”»’è
 	bool LineCrossPoint(float a1x, float a1y, float a2x, float a2y, float b1x, float b1y, float b2x, float b2y, float* out_px, float* out_py);
 
-
-
-
-
 };
