db6585b2840316665e0828ea0c7c43547f6bf00b
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-11-10 16:35:33 +0900

    iroiro

---
+++ U_AD2/ObjBackground.cpp
@@ -0,0 +1,26 @@
+//使用するヘッダーファイル
+#include"GameL\DrawTexture.h"
+#include"GameHead.h"
+
+#include"ObjBackground.h"
+
+//使用してる加茂野のネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjBackground::Init()
+{
+
+}
+
+//アクション
+void CObjBackground::Action()
+{
+
+}
+
+//ドロー
+void CObjBackground::Draw()
+{
+
+}
