9c6f83943a9c48bfab78222869f6845b41acd0e0
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-11-10 15:10:27 +0900

    ゲームオーバー文字変更

--- U_AD2/SceneGameOver.cpp
+++ U_AD2/SceneGameOver.cpp
@@ -30,6 +30,7 @@ void CSceneGameOver::InitScene()
 {
 	//出力させる文字のグラフィックを作成
 	Font::SetStrTex(L"GameOver");
+	Font::SetStrTex(L"がんばれ");
 	Font::SetStrTex(L"NEXT_CHALLENGE:ENTER_KEY");
 
 	//ゲームオーバーオブジェクト作成
