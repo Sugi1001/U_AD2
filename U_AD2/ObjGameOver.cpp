4b45d5cb140c714c4c4a6b3c7b4f90264a10a8ac
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-11-11 10:35:29 +0900

    文字変更でっかくなっちゃった　きゃはっ

--- U_AD2/ObjGameOver.cpp
+++ U_AD2/ObjGameOver.cpp
@@ -36,7 +36,8 @@ void CObjGameOver::Action()
 void CObjGameOver::Draw()
 {
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
-	Font::StrDraw(L"GameOver　ゲ～ムオ～バ～", 230, 190 , 32, c);
-	Font::StrDraw(L"あきらめていいんですか?", 215, 250, 32, c);
-	Font::StrDraw(L"ENTER_KEYでタイトルに戻るよ", 200, 400, 32, c);
+	Font::StrDraw(L"GameOver　ゲ～ムオ～バ～", 120, 190 , 50, c);
+	Font::StrDraw(L"あきらめていいんですか?", 215, 275, 32, c);
+	Font::StrDraw(L"タイトルから出直してきてくださいまし", 150, 330, 32, c);
+	Font::StrDraw(L"ENTER_KEYでタイトルに戻るよ", 100, 400, 50, c);
 }
