43d5189849cac834234b9bfed0c33232735dfe22
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-11-11 10:56:29 +0900

    タイトル

--- U_AD2/ObjHero.cpp
+++ U_AD2/ObjHero.cpp
@@ -60,7 +60,7 @@ void CObjHero::Action()
 	if (m_py > 1000.0f)
 	{
 	//場外に出たらリスタート
-		Scene::SetScene((new CSceneGameOver()));//CSceneMain
+		Scene::SetScene((new CSceneGameOver()));//CSceneGameOver
 	
 	}
 
