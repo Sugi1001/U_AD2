730958541daac10d477c41c323675ffebcb7af15
愛斗 小林 <h1202015020@hiratagakuen.onmicrosoft.com>
2020-11-18 11:53:50 +0900

    時は再び巻き戻る

--- U_AD2/ObjHero.cpp
+++ U_AD2/ObjHero.cpp
@@ -16,11 +16,11 @@ using namespace GameL;
 void CObjHero::Init()
 {
 
-	m_px = 0.0f; //位置
+	m_px = 1.0f; //位置
 	m_py = 0.0f;
-	m_vx = 0.0f;//移動ベクトル
+	m_vx = 1.0f;//移動ベクトル
 	m_vy = 0.0f;
-	m_posture =1.0f; //右向き0.0f　左向き1.0f
+	m_posture = 1.0f; //右向き0.0f　左向き1.0f
 
 	m_ani_time = 0;
 	m_ani_frame = 1;//静止フレームを初期にする
@@ -34,7 +34,7 @@ void CObjHero::Init()
 	m_hit_left = false;
 	m_hit_right = false;
 
-	
+
 
 	m_block_type = 0;
 
@@ -55,12 +55,12 @@ void CObjHero::Action()
 		//m_hp -= 2;
 	//}
 	//移動ベクトルの破棄
-	 
+
 	if (m_py > 1000.0f)
 	{
-	//場外に出たらリスタート
+		//場外に出たらリスタート
 		Scene::SetScene((new CSceneGameOver()));//CSceneGameOver
-	
+
 	}
 
 	////当たり判定を行うオブジェクト情報部
@@ -91,22 +91,22 @@ void CObjHero::Action()
 
 
 
-	
+
 	if (Input::GetVKey('X') == true)
 	{
 		if (m_hit_down == true)
 		{
-			m_vy = - 20;
+			m_vy = -20;
 		}
 	}
-	
+
 	////キーの入力方向
 	if (Input::GetVKey(VK_LEFT) == true)
 	{
-		m_vx=+1.5f;
-		m_vx=-5.0f;
+		m_vx = +1.5f;
+		m_vx = -5.0f;
 		m_posture = 1.5f;
-		m_ani_time+=1;
+		m_ani_time += 1;
 	}
 	if (m_ani_time > 4)
 	{
@@ -118,19 +118,19 @@ void CObjHero::Action()
 		m_ani_frame = 0;
 	}
 
-    else if (Input::GetVKey(VK_RIGHT) == true)
+	else if (Input::GetVKey(VK_RIGHT) == true)
 	{
-		m_vx=-1.5f;
-		m_vx=+3.0f;
+		m_vx = -1.5f;
+		m_vx = +3.0f;
 		m_posture = 0.0f;
-		m_ani_time+=1;
+		m_ani_time += 1;
 	}
 	else
 	{
 		m_ani_frame = 1; //キーが入力が無い場合静止フレームにする
 		m_ani_time = 1;
 	}
-	
+
 
 	//摩擦
 	m_vx += -(m_vx * 0.098);
@@ -144,23 +144,23 @@ void CObjHero::Action()
 		&m_hit_up, &m_hit_down, &m_hit_left, &m_hit_right, &m_vx, &m_vy,
 		&m_block_type
 
-		
+
 
 	);
-       
-////棘が主人公に接触したら主人公のHPを減らす
-//	if (hit->CheckObjNameHit(OBJ_TOGE) != nullptr)
-//	{
-//		m_hp -= 2;
-//	}
 
-	
-	//位置の更新
+	////棘が主人公に接触したら主人公のHPを減らす
+	//	if (hit->CheckObjNameHit(OBJ_TOGE) != nullptr)
+	//	{
+	//		m_hp -= 2;
+	//	}
+
+
+		//位置の更新
 	m_px += m_vx;
 	m_py += m_vy;
 
 	//主人公の位置X（ｍ＿ｘ）+主人公機の幅分がX軸方向に領域外を認識
-	if (m_px + 30.0f > 500.0f)
+	if (m_px + 20.0f > 800.0f)
 	{
 		m_px = 800.0f - 20.0f;
 	}
@@ -186,22 +186,18 @@ void CObjHero::Draw()
 
 
 	//表示位置の設定
-	dst.m_top = 0.0f+m_py;
-	dst.m_left =(     0.0f*m_posture)+m_px;
-	dst.m_right =( 60  -   50.0f*m_posture)+m_px;
+	dst.m_top = 0.0f + m_py;
+	dst.m_left = (40.0f * m_posture) + m_px;
+	dst.m_right = (64 - 50.0f * m_posture) + m_px;
 	dst.m_bottom = 64.0 + m_py;
 
 	//表示位置の設定
 	dst.m_top = 0.0f + m_py;
 	dst.m_left = (40.0f * m_posture) + m_px;
-	dst.m_right = (60 - 50.0f * m_posture) + m_px;
+	dst.m_right = (64 - 50.0f * m_posture) + m_px;
 	dst.m_bottom = 64.0 + m_py;
 
 	//描画
 	Draw::Draw(1, &src, &dst, c, 1.0f);
 
 }
-	
-	
-
-
