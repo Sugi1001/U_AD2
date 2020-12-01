cf79ac12f260611c36f34b316f4aed245b70938e
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-12-01 13:51:29 +0900

    「a」

--- U_AD2/ObjStageBlock.cpp
+++ U_AD2/ObjStageBlock.cpp
@@ -141,7 +141,61 @@ void CObjStageBlock::Action()
 			}
 		}
 	}
+for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 500; j++) {
+			if (m_map[i][j] == 6) {
+				float x = j * 64.0f;
+				float y = i * 64.0f;
 
+				if ((hx + (-m_scroll) + 64.0f > x) && (hx + (-m_scroll) < x + 64.0f) && (hy + 64.0f > y) && (hy < y + 64.0f)) {
+					float vx = (hx + (-m_scroll)) - x;
+					float vy = hy - y;
+
+					float len = sqrt(vx * vx + vy * vy);
+
+					float r = atan2(vy, vx);
+					r = r * 180.0f / 3.14f;
+
+					if (r <= 0.0f)
+						r = abs(r);
+					else
+						r = 360.0f - abs(r);
+
+					if (len < 88.0f) {
+						if ((r < 45 && r > 0) || r > 315) {
+							hero->SetRight(true);
+							hero->SetX(x + 64.0f + (m_scroll));
+							hero->SetVX(-hero->GetVX() * 0.1f);
+						}
+						if (r > 45 && r < 135) {
+							hero->SetDown(true);
+							hero->SetY(y - 64.0f);
+							if (m_map[i][j] >= 2)
+								hero->SetBT(m_map[i][j]);
+							hero->SetVY(0.0f);
+						}
+						if (r > 135 && r < 225) {
+							hero->SetLeft(true);
+							hero->SetX(x - 64.0f + (m_scroll));
+							hero->SetVX(-hero->GetVX() * 0.1f);
+						}
+						if (r > 255 && r < 315) {
+							hero->SetUp(true);
+							hero->SetY(y + 64.0f);
+							if (hero->GetVY() < 1) {
+								hero->SetVY(0.0f);
+							}
+						}
+					}
+				}
+
+				else
+				{
+
+				}
+			}
+		}
+	}
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 500; j++) {
 			if (m_map[i][j] >= 8) {
@@ -198,61 +252,7 @@ void CObjStageBlock::Action()
 			}
 		}
 	}
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 500; j++) {
-			if (m_map[i][j] >= 6) {
-				float x = j * 64.0f;
-				float y = i * 64.0f;
-
-				if ((hx + (-m_scroll) + 64.0f > x) && (hx + (-m_scroll) < x + 64.0f) && (hy + 64.0f > y) && (hy < y + 64.0f)) {
-					float vx = (hx + (-m_scroll)) - x;
-					float vy = hy - y;
-
-					float len = sqrt(vx * vx + vy * vy);
-
-					float r = atan2(vy, vx);
-					r = r * 180.0f / 3.14f;
-
-					if (r <= 0.0f)
-						r = abs(r);
-					else
-						r = 360.0f - abs(r);
-
-					if (len < 88.0f) {
-						if ((r < 45 && r > 0) || r > 315) {
-							hero->SetRight(true);
-							hero->SetX(x + 64.0f + (m_scroll));
-							hero->SetVX(-hero->GetVX() * 0.1f);
-						}
-						if (r > 45 && r < 135) {
-							hero->SetDown(true);
-							hero->SetY(y - 64.0f);
-							if (m_map[i][j] >= 2)
-								hero->SetBT(m_map[i][j]);
-							hero->SetVY(0.0f);
-						}
-						if (r > 135 && r < 225) {
-							hero->SetLeft(true);
-							hero->SetX(x - 64.0f + (m_scroll));
-							hero->SetVX(-hero->GetVX() * 0.1f);
-						}
-						if (r > 255 && r < 315) {
-							hero->SetUp(true);
-							hero->SetY(y + 64.0f);
-							if (hero->GetVY() < 1) {
-								hero->SetVY(0.0f);
-							}
-						}
-					}
-				}
-
-				else
-				{
-
-				}
-			}
-		}
-	}
+	
 
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 500; j++) {
@@ -344,7 +344,6 @@ void CObjStageBlock::Action()
 								if (m_map[i][j] >= 2)
 									hero->SetBT(m_map[i][j]);
 								hero->SetVY(0.0f);
-								Scene::SetScene((new CSceneGameOver()));
 							}
 							if (r > 135 && r < 225) {
 								hero->SetLeft(true);
@@ -496,6 +495,15 @@ void CObjStageBlock::Draw()
 					//BlockDraw(320.0f, 0.0f, &dst, c);
 				}
 				else if (m_map[i][j] == 8)
+				{
+					src.m_top = 0.0f;
+					src.m_left = 320.0f + 64.0;
+					src.m_right = src.m_left + 64.0f;
+					src.m_bottom = src.m_top + 64.0f;
+					Draw::Draw(50, &src, &dst, c, 0.0f);
+					//BlockDraw(320.0f, 0.0f, &dst, c);
+				}
+				else if (m_map[i][j] == 9)
 				{
 					src.m_top = 0.0f;
 					src.m_left = 320.0f + 64.0;
