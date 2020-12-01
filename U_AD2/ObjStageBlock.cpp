1a73c0fa18ddbe0a6daf5573f22b176de3ee2170
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-12-01 15:52:49 +0900

    a

--- U_AD2/ObjStageBlock.cpp
+++ U_AD2/ObjStageBlock.cpp
@@ -60,10 +60,10 @@ void CObjStageBlock::Action()
 
 
 	//主人公衝突判定
-	//hero->SetUp(false);
-	//hero->SetDown(false);
-	//hero->SetLeft(false);
-	//hero->SetRight(false);
+	hero->SetUp(false);
+	hero->SetDown(false);
+	hero->SetLeft(false);
+	hero->SetRight(false);
 	//後方スクロールライン
 	if (hx < 0)
 	{
