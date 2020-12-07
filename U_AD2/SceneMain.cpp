51206b66ad86a39d1180dd86b5c8d1f6e2874442
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-12-07 13:40:53 +0900

    a

--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -44,7 +44,7 @@ void CSceneMain::InitScene()
 	//Audio::Start(0); //音楽スタート
 
 	//Font作成
-	Font::SetStrTex(L"0123456789分秒");
+	//Font::SetStrTex(L"0123456789分秒");
 	//外部データの読み込み（ステージ情報）
 	unique_ptr<wchar_t>p;//ステージ情報ポインター
 	int size;//ステージ情報の大きさ
