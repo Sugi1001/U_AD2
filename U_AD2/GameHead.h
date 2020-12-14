de97e6bf80a556df3f57d866ba08b1003306f9bd
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2020-12-14 13:31:04 +0900

    ステージのプログラム追加

--- U_AD2/GameHead.h
+++ U_AD2/GameHead.h
@@ -13,7 +13,7 @@ enum OBJ_NAME
 	//OBJ_HP,
 	//OBJ_BLOCK,
 	OBJ_STAGE_BLOCK,
-	OBJ_STAGE_BLOCK2,
+	OBJ_STAGE_BLOCK1,
 	OBJ_BLOCK,
 
 	OBJ_TITLE,
@@ -68,7 +68,7 @@ struct UserData
 //ゲームシーンオブジェクトヘッダ------------------
 #include "ObjHero.h"
 #include"ObjStageBlock.h"
-#include"ObjStageBlock2.h"
+#include"ObjStageBlock1.h"
 #include "ObjEnemyBlock.h"
 
 #include  "CObjToge.h"
