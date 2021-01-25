c53704abf4bb2ef657a9f5e8cbd198699eda50dd
友一 杉下 <h1202019001@hiratagakuen.onmicrosoft.com>
2021-01-25 14:34:35 +0900

    プログラムがキレイになりました.

--- U_AD2/ObjStageBlock1.cpp
+++ U_AD2/ObjStageBlock1.cpp
@@ -784,7 +784,7 @@ bool CObjStageBlock1::HeroBlockCrossPoint(float x, float y, float vx, float vy, 
 					//交点チェック
 					if (b == true)
 					{
-						//杉下との距離を求める、だるいから
+						
 						l = sqrt((px - x) * (px - x) + (py - y) * (py - y));
 
 						//見つけた交差の中でも最も距離が短いモノを探す
