0d70cb4b477b1d9975edc1bdc0fcfbe65628eea6
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2021-01-25 15:42:18 +0900

    いろいろ変更

--- U_AD2/SceneMain.cpp
+++ U_AD2/SceneMain.cpp
@@ -76,6 +76,7 @@ void CSceneMain::InitScene()
 	Draw::LoadImage(L"こうもり.png", 6, TEX_SIZE_512);
 	Draw::LoadImage(L"こうもり（移動）.png", 7, TEX_SIZE_512);
 	Draw::LoadImage(L"氷柱.png", 6, TEX_SIZE_512);
+	Draw::LoadImage(L"棘.png", 8, TEX_SIZE_512);
 	Draw::LoadImage(L"岩2.png", 9, TEX_SIZE_512);
 	Draw::LoadImage(L"氷床.png", 20, TEX_SIZE_512);
 	Draw::LoadImage(L"宝箱（重要アイテム）.png", 50, TEX_SIZE_512);
