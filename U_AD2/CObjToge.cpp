c1e3e6213ab9f6abefb9928ae2abcc7aa480386c
猛 岩永 <h1202015006@hiratagakuen.onmicrosoft.com>
2021-01-25 14:37:16 +0900

    komitto

--- U_AD2/CObjToge.cpp
+++ U_AD2/CObjToge.cpp
@@ -87,9 +87,9 @@ void CObjToge::Draw()
 	
 	//表示位置の設定
 	dst.m_top = 0.0f + m_py;
-	dst.m_left = (50.0f * m_posture) + m_px;
-	dst.m_right = (50 + 50.0f * m_posture) + m_px;
-	dst.m_bottom = 50.0f + m_py;
+	dst.m_left = (100.0f * m_posture) + m_px;
+	dst.m_right = (100 + 100.0f * m_posture) + m_px;
+	dst.m_bottom = 100.0f + m_py;
 
 	//3番目に登録したグラフィックをsrc.dst.cの情報を元に描画
 	Draw::Draw(20, &src, &dst, c, 0.0f);
