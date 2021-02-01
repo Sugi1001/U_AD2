9d07ce642377951c44cef70efb3294733c291baa
隆平 藤田 <h1202015011@hiratagakuen.onmicrosoft.com>
2021-02-01 16:21:49 +0900

    操作説明の文字位置変更

--- U_AD2/ObjGameSousa.cpp
+++ U_AD2/ObjGameSousa.cpp
@@ -45,6 +45,6 @@ void CObjGameSousa::Draw()
 	Font::StrDraw(L"←キー　後ろに進む", 150, 250, 60, c);
 	Font::StrDraw(L"Xキー　ジャンプ", 150, 350, 60, c);
 
-	Font::StrDraw(L"焦らず丁寧に進むといいですよ（経験談）", 55, 460, 40, c);
-	Font::StrDraw(L"アイテムもあるから攻略に役立ててね！", 80, 510, 40, c);
+	Font::StrDraw(L"焦らず丁寧に進むといいですよ（経験談）", 35, 460, 40, c);
+	Font::StrDraw(L"アイテムもあるから攻略に役立ててね！", 60, 510, 40, c);
 }
