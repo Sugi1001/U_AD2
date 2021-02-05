65b397bb3179c72b61cd84f1a374394004f97394
真優 山口 <h1202015017@hiratagakuen.onmicrosoft.com>
2021-02-05 09:27:36 +0900

    ゴール位置修正

--- U_AD2/ObjStageBlock1.cpp
+++ U_AD2/ObjStageBlock1.cpp
@@ -65,7 +65,7 @@ void CObjStageBlock1::Action()
 	}
 
 	//中間の設定（途中）
-	for (int i = 0; i < 10; i++) {
+	/*for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 128; j++) {
 			if (m_map[i][j] == 9) {
 				float x = j * 64.0f;
@@ -125,7 +125,7 @@ void CObjStageBlock1::Action()
 				}
 			}
 		}
-	}
+	}*/
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 250; j++) {
 			if (m_map[i][j] == 1) {
@@ -180,7 +180,63 @@ void CObjStageBlock1::Action()
 		}
 
 	}
-	//スタート位置の設定
+	//ゴール位置の設定
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 250; j++) {
+			if (m_map[i][j] == 2) {
+				float x = j * 64.0f;
+				float y = i * 64.0f;
+
+				if ((hx + (-m_scroll) + 64.0f > x) && (hx + (-m_scroll) < x + 64.0f) && (hy + 64.0f > y) && (hy < y + 64.0f)) {
+					float vx = (hx + (-m_scroll)) - x;
+					float vy = hy - y;
+
+					float len = sqrt(vx * vx + vy * vy);
+
+					float r = atan2(vy, vx);
+					r = r * 180.0f / 3.14f;
+
+					if (r <= 0.0f)
+						r = abs(r);
+					else
+						r = 360.0f - abs(r);
+
+					if (len < 88.0f) {
+						if ((r < 45 && r > 0) || r > 315) {
+							hero->SetRight(true);
+							hero->SetX(x + 64.0f + (m_scroll));
+							hero->SetVX(-hero->GetVX() * 0.1f);
+						}
+						if (r > 45 && r < 135) {
+							hero->SetDown(true);
+							hero->SetY(y - 64.0f);
+							if (m_map[i][j] >= 2)
+								hero->SetBT(m_map[i][j]);
+							hero->SetVY(0.0f);
+						}
+						if (r > 135 && r < 225) {
+							hero->SetLeft(true);
+							hero->SetX(x - 64.0f + (m_scroll));
+							hero->SetVX(-hero->GetVX() * 0.1f);
+							Scene::SetScene((new CSceneClear()));
+						}
+						if (r > 255 && r < 315) {
+							hero->SetUp(true);
+							hero->SetY(y + 64.0f);
+							if (hero->GetVY() < 1) {
+								hero->SetVY(0.0f);
+							}
+						}
+					}
+				}
+
+				else
+				{
+
+				}
+			}
+		}
+	}
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 250; j++) {
 			if (m_map[i][j] == 6) {
@@ -237,67 +293,10 @@ void CObjStageBlock1::Action()
 			}
 		}
 	}
-	//ゴール＆クリア画面への移行の設定
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 250; j++) {
-			if (m_map[i][j] == 8) {
-				float x = j * 64.0f;
-				float y = i * 64.0f;
-
-				if ((hx + (-m_scroll) + 64.0f > x) && (hx + (-m_scroll) < x + 64.0f) && (hy + 64.0f > y) && (hy < y + 64.0f)) {
-					float vx = (hx + (-m_scroll)) - x;
-					float vy = hy - y;
-
-					float len = sqrt(vx * vx + vy * vy);
-
-					float r = atan2(vy, vx);
-					r = r * 180.0f / 3.14f;
-
-					if (r <= 0.0f)
-						r = abs(r);
-					else
-						r = 360.0f - abs(r);
-
-					if (len < 88.0f) {
-						if ((r < 45 && r > 0) || r > 315) {
-							hero->SetRight(true);
-							hero->SetX(x + 64.0f + (m_scroll));
-							hero->SetVX(-hero->GetVX() * 0.1f);
-						}
-						if (r > 45 && r < 135) {
-							hero->SetDown(true);
-							hero->SetY(y - 64.0f);
-							if (m_map[i][j] >= 2)
-								hero->SetBT(m_map[i][j]);
-							hero->SetVY(0.0f);
-						}
-						if (r > 135 && r < 225) {
-							hero->SetLeft(true);
-							hero->SetX(x - 64.0f + (m_scroll));
-							hero->SetVX(-hero->GetVX() * 0.1f);
-							Scene::SetScene((new CSceneClear()));
-						}
-						if (r > 255 && r < 315) {
-							hero->SetUp(true);
-							hero->SetY(y + 64.0f);
-							if (hero->GetVY() < 1) {
-								hero->SetVY(0.0f);
-							}
-						}
-					}
-				}
-
-				else
-				{
-
-				}
-			}
-		}
-	}
 	//棘の上部に触れると死ぬ設定
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 250; j++) {
-			if (m_map[i][j] >= 7) {
+			if (m_map[i][j] == 7) {
 				float x = j * 64.0f;
 				float y = i * 64.0f;
 
