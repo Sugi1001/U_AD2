1b043d9ee9fdb3907d144e3e8bf2d72b00a527fc
隆平 藤田 <h1202015011@hiratagakuen.onmicrosoft.com>
2021-02-08 13:59:01 +0900

    b

--- U_AD2/ObjGameSousa.cpp
+++ U_AD2/ObjGameSousa.cpp
@@ -43,7 +43,7 @@ void CObjGameSousa::Draw()
 
 	Font::StrDraw(L"→キー　前に進む", 150, 150, 60, c);
 	Font::StrDraw(L"←キー　後ろに進む", 150, 250, 60, c);
-	Font::StrDraw(L"Xキー　ジャンプ", 150, 350, 60, c);
+	Font::StrDraw(L"Xキー　ジャンプ", 178, 350, 60, c);
 
 	Font::StrDraw(L"焦らず丁寧に進むといいですよ（経験談）", 35, 460, 40, c);
 	Font::StrDraw(L"エンターキーでゲームスタート", 150, 510, 40, c);
